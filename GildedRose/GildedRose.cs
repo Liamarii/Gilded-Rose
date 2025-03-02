effec95550563d06b454da33a8312694d31b55ff
Liam Costello <liamdcostello@gmail.com>
2025-03-02 14:21:44 +0000

    Creating a dictioary of the strategies.

--- GildedRose/GildedRose.cs
+++ GildedRose/GildedRose.cs
@@ -12,6 +12,60 @@ public class GildedRose
         this.Items = Items;
     }
 
+
+    public interface IStrategy
+    {
+        public void Process(Item item);
+    }
+
+    public class AgedStrategy : IStrategy
+    {
+        public void Process(Item item)
+        {
+            item.IncreaseTheQualityIfConditionMet(item.Quality < 50);
+            item.IncreaseTheQualityIfConditionMet(item.SellIn < 1 && item.Quality < 50);
+            item.SellIn--;
+        }
+    }
+
+    public class BackStageStrategy : IStrategy
+    {
+        public void Process(Item item)
+        {
+            item.IncreaseTheQualityIfConditionMet(item.Quality < 50);
+            item.IncreaseTheQualityIfConditionMet(item.SellIn < 11 && item.Quality < 50);
+            item.IncreaseTheQualityIfConditionMet(item.SellIn < 6 && item.Quality < 50);
+            item.DecreaseQualityIfConditionsMet(--item.SellIn < 0);
+        }
+    }
+
+    public class DefaultStrategy : IStrategy
+    {
+        public void Process(Item item)
+        {
+            item.LowerItemQualityIfConditionMet(item.Quality > 0);
+            item.SellIn--;
+            item.LowerItemQualityIfConditionMet(item.Quality > 0 && item.SellIn < 0);
+        }
+    }
+
+    public class SulfurasStrategy : IStrategy
+    {
+        public void Process(Item item)
+        {
+        }
+    }
+
+    public Dictionary<string, IStrategy> Strategies = new()
+    {
+        { "Aged Brie", new AgedStrategy() },
+        { "Backstage passes to a TAFKAL80ETC concert", new BackStageStrategy() },
+        { "Default", new DefaultStrategy() },
+        { "Sulfuras, Hand of Ragnaros", new SulfurasStrategy()}
+    };
+
+
+
     public void UpdateQuality()
     {
         foreach (Item item in Items)
