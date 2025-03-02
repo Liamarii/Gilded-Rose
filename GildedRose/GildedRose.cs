decafb7c01c0ba364c4db035f58a5210c337a592
Liam Costello <liamdcostello@gmail.com>
2025-03-02 11:39:44 +0000

    Inverted all the name ifs to read positively.

--- GildedRose/GildedRose.cs
+++ GildedRose/GildedRose.cs
@@ -15,17 +15,33 @@ public class GildedRose
     {
         foreach (Item item in Items)
         {
-            if (item.Name != "Aged Brie" && item.Name != "Backstage passes to a TAFKAL80ETC concert")
+            if (item.Name == "Aged Brie")
             {
-                if (item.Quality > 0)
+                if (item.Quality < 50)
                 {
-                    if (item.Name != "Sulfuras, Hand of Ragnaros")
+                    item.Quality = item.Quality + 1;
+
+                    if (item.Name == "Backstage passes to a TAFKAL80ETC concert")
                     {
-                        item.Quality = item.Quality - 1;
+                        if (item.SellIn < 11)
+                        {
+                            if (item.Quality < 50)
+                            {
+                                item.Quality = item.Quality + 1;
+                            }
+                        }
+
+                        if (item.SellIn < 6)
+                        {
+                            if (item.Quality < 50)
+                            {
+                                item.Quality = item.Quality + 1;
+                            }
+                        }
                     }
                 }
             }
-            else
+            else if (item.Name == "Backstage passes to a TAFKAL80ETC concert")
             {
                 if (item.Quality < 50)
                 {
@@ -52,35 +68,53 @@ public class GildedRose
                 }
             }
 
-            if (item.Name != "Sulfuras, Hand of Ragnaros")
+            else
+            {
+                if (item.Quality > 0)
+                {
+                    if (item.Name == "Sulfuras, Hand of Ragnaros")
+                    {
+                    }
+                    else
+                    {
+                        item.Quality = item.Quality - 1;
+                    }
+                }
+            }
+
+            if (item.Name == "Sulfuras, Hand of Ragnaros")
+            {
+            }
+            else
             {
                 item.SellIn = item.SellIn - 1;
             }
 
             if (item.SellIn < 0)
             {
-                if (item.Name != "Aged Brie")
-                {
-                    if (item.Name != "Backstage passes to a TAFKAL80ETC concert")
-                    {
-                        if (item.Quality > 0)
-                        {
-                            if (item.Name != "Sulfuras, Hand of Ragnaros")
-                            {
-                                item.Quality = item.Quality - 1;
-                            }
-                        }
-                    }
-                    else
-                    {
-                        item.Quality = item.Quality - item.Quality;
-                    }
-                }
-                else
+                if (item.Name == "Aged Brie")
                 {
                     if (item.Quality < 50)
                     {
                         item.Quality = item.Quality + 1;
+                    }
+                }
+                else
+                {
+                    if (item.Name == "Backstage passes to a TAFKAL80ETC concert")
+                    {
+                        item.Quality = item.Quality - item.Quality;
+                    }
+                    else
+                    {
+                        if (item.Quality > 0)
+                        {
+                            if (item.Name == "Sulfuras, Hand of Ragnaros")
+                            {
+                                continue;
+                            }
+                            item.Quality = item.Quality - 1;
+                        }
                     }
                 }
             }
