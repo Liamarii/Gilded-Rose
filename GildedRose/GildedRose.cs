53bb9d9217b1c15c39b4935f3601a81603f36c40
Liam Costello <liamdcostello@gmail.com>
2025-03-02 11:53:35 +0000

    got rid of the last duplicate name branch to have one if name tree.

--- GildedRose/GildedRose.cs
+++ GildedRose/GildedRose.cs
@@ -52,33 +52,27 @@ public class GildedRose
                     }    
                 }
                 item.SellIn = item.SellIn - 1;
+                if (item.SellIn < 0)
+                {
+                    if (item.Name == "Backstage passes to a TAFKAL80ETC concert")
+                    {
+                        item.Quality = item.Quality - item.Quality;
+                    }
+                    else
+                    {
+                        if (item.Quality > 0)
+                        {
+                            if (item.Name == "Sulfuras, Hand of Ragnaros")
+                            {
+                                continue;
+                            }
+                            item.Quality = item.Quality - 1;
+                        }
+                    }
+                }
             }
             
             else if (item.Name == "Sulfuras, Hand of Ragnaros")
-            {
-            }
- 
-            else
-            {
-                if (item.Quality > 0)
-                {
-                    item.Quality = item.Quality - 1;
-                }
-                item.SellIn = item.SellIn - 1;
-            } 
-        
-
-
-
-
-
-
-
-
-
-
-
-            if (item.Name != "Aged Brie")
             {
                 if (item.SellIn < 0)
                 {
@@ -98,8 +92,34 @@ public class GildedRose
                         }
                     }
                 }
-            
             }
+ 
+            else
+            {
+                if (item.Quality > 0)
+                {
+                    item.Quality = item.Quality - 1;
+                }
+                item.SellIn = item.SellIn - 1;
+                if (item.SellIn < 0)
+                {
+                    if (item.Name == "Backstage passes to a TAFKAL80ETC concert")
+                    {
+                        item.Quality = item.Quality - item.Quality;
+                    }
+                    else
+                    {
+                        if (item.Quality > 0)
+                        {
+                            if (item.Name == "Sulfuras, Hand of Ragnaros")
+                            {
+                                continue;
+                            }
+                            item.Quality = item.Quality - 1;
+                        }
+                    }
+                }
+            } 
         }
     }
 }
